719d0657570db4300669c77ea6ca4bfc2acae813
dr0se-mirror <2721808339@qq.com>
2025-01-03 16:41:01 +0800

    简化ql规则库

--- ql/csharp/ql/integration-tests/all-platforms/standalone_resx/test.py
+++
@@ -1,6 +0,0 @@
-import os
-from create_database_utils import *
-
-os.environ["CODEQL_EXTRACTOR_CSHARP_BUILDLESS_EXTRACT_RESOURCES"] = "true"
-
-run_codeql_database_create(lang="csharp", extra_args=["--build-mode=none"])
